fa812f756d0ae8cc3ce9d48c660ff958f09f5d4d
reveke-hp <65628133+reveke-hp@users.noreply.github.com>
2021-09-19 09:28:41 -0300

    Add files via upload

---
+++ Ejercicio 1 - Closures/scripts.js
@@ -0,0 +1,11 @@
+function cambiarDimensionFuente(size) {
+    return function() { 
+        document.body.style.fontSize = size + 'px';
+        };
+}
+
+function setClicks(){
+    document.getElementById('fuente-8').onclick = cambiarDimensionFuente(8);
+    document.getElementById('fuente-16').onclick = cambiarDimensionFuente(16);
+    document.getElementById('fuente-24').onclick = cambiarDimensionFuente(24);
+}
